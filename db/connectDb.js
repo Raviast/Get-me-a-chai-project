d0f06d8892778dec47ad038c333dde7dd6d4fd64
Raviast <raviastare6@gmail.com>
2024-12-05 20:50:53 +0530

    database connection

--- db/connectDb.js
+++ db/connectDb.js
@@ -3,16 +3,17 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
         try {
-            const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
+            const conn = await mongoose.connect(`mongodb+srv://sunilbejesh:KuZvebx1DpgiPSeM@cluster0.ngfwe.mongodb.net/chai?retryWrites=true&w=majority&appName=Cluster0`, {
                 useNewUrlParser: true,
             });
             console.log(`MongoDB Connected: ${conn.connection.host}`);
             return conn;
             
         } catch (error) {
-            console.error(error.message);
-            process.exit(1);
+            console.error("Error while db connection",error.message);
+            // process.exit(1);
         }
     }
 
   export default connectDb;
+  
